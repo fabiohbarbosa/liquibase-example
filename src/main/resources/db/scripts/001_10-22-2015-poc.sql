9b25b4f5a203ee672c8bcd60585862d85d59eef8
Fabio H. G. Barbosa <fabiohbarbosa@gmail.com>
2015-10-22 11:46:28 -0200

    Create liquibase changelog config file and script example

---
+++ src/main/resources/db/scripts/001_10-22-2015-poc.sql
@@ -0,0 +1,15 @@
+--liquibase formatted sql
+--changeset fabio.barbosa:001.1
+CREATE SEQUENCE POC_SEQUENCE MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20;
+CREATE TABLE POC (
+  ID NUMBER(19) NOT NULL,
+  NAME VARCHAR2(255) NULL
+);
+--rollback DROP SEQUENCE POC_SEQUENCE;
+--rollback DROP TABLE POC;
+
+--liquibase formatted sql
+--changeset fabio.barbosa:001.2 context:tests
+INSERT INTO POC VALUES(POC_SEQUENCE.nextval, 'Poc Test 1');
+INSERT INTO POC VALUES(POC_SEQUENCE.nextval, 'Poc Test 2');
+--rollback DELETE FROM POC WHERE NAME in('Poc Test 1', 'Poc Test 2');
